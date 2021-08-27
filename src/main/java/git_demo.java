889d6ff98691a41493d358c892e0d3e14c6f6d0e
ls <liu255765@163.com>
2021-08-27 21:14:45 +0800

    first commit

---
+++ src/main/java/git_demo.java
@@ -0,0 +1,12 @@
+/**
+ * @author 刘帅
+ * @create 2021-08-27 21:08
+ */
+
+
+public class git_demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
+
